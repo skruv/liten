6c1241d2aff9f306006982a346765814a9301219
Svante Richter <svante.richter@gmail.com>
2024-09-22 23:09:15 +0200

    tmp

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const domCache = new WeakMap()
-export const e = new Proxy({}, { get: (_, t) => (...c) => (c[0] && typeof c[0] === 'object' && !c[0].t) ? { t, a: c[0], c: c.slice(1) } : { a: null, t, c } })
-export const r = (object, current = document.documentElement, parent = current.parentNode) => {
-  if (!current || current.localName !== object.t) {
-    let _current = document.createElement(object.t)
-    domCache.set(object, _current)
-    current ? parent.replaceChild(_current, current) : parent.appendChild(_current)
-    current = _current
-  }
+const domCache = {}
+const cache = new WeakMap()
+export const e = new Proxy({}, { get: (_, t) => (...c) => (c[0] && typeof c[0] === 'object' && !c[0].t) ? { t, a: c[0], c: c.slice(1) } : { t, c } })
+export const r = (object, current = document.documentElement) => {
   if (object.a) {
     for (const key in object.a) {
-      if (('' + object.a[key]) === ('' + current.getAttribute(key))) { continue }
+      if (key === 'key' || key === 'cache' || ('' + object.a[key]) === ('' + current.getAttribute(key))) continue
       if (key[0] === 'o' && key[1] === 'n') { // Event listener
         current[key] = object.a[key]
         continue
@@ -24,25 +19,41 @@ export const r = (object, current = document.documentElement, parent = current.p
       }
     }
   }
-  if (!object.c.length && current.childNodes.length) {
-    current.textContent = ''
-    return
-  }
+  if (!object.c.length && current.childNodes.length) current.textContent = ''
+  if (!object.c.length) return
   for (const i in object.c) {
     const c = object.c[i]
-    const p = current.childNodes[i] || null
+    let p = current.childNodes[i] || null
     if (typeof c === 'string' || typeof c === 'number') {
       if (!p || p.nodeName !== '#text') {
         p ? current.replaceChild(document.createTextNode(c), p) : current.appendChild(document.createTextNode(c))
-      } else if (p.nodeValue !== ('' + c)) {
+      } else if (p.nodeValue !== (typeof c === 'string' ? c : '' + c)) {
         p.nodeValue = c
       }
-    } else if (domCache.has(c)) {
-      let _current = domCache.get(c).cloneNode(true)
-      p ? current.replaceChild(_current, c) : current.appendChild(_current)
-    } else {
-      r(c, p, current)
+      continue
     }
+    if (c.a?.cached && cache.has(c)) {
+      if (cache.get(c) === p) continue
+      let _current = cache.get(c).cloneNode(true)
+      p ? current.replaceChild(_current, p) : current.appendChild(_current)
+      continue
+    }
+    if (c.a?.key && cache.has(c.a.key)) {
+      if (cache.get(c.a.key) === p) continue
+      let _current = cache.get(c.a.key)
+      p ? current.replaceChild(_current, p) : current.appendChild(_current)
+      p = _current
+      continue
+    }
+    if (!p || p.localName !== c.t) {
+      let _current = (domCache[c.t] || (domCache[c.t] = document.createElement(c.t))).cloneNode(false)
+      c.a?.cached && cache.set(c, _current)
+      c.a?.key && cache.set(c.a.key, _current) && cache.set(_current, c.a.key)
+      p ? current.replaceChild(_current, p) : current.appendChild(_current)
+      r(c, _current)
+      continue
+    }
+    r(c, p)
   }
   while (current.childNodes.length > object.c.length) {
     current.removeChild(current.childNodes[current.childNodes.length - 1])
