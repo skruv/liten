4e282d0dd19da2fa9d23af91a190a01f20fc21cf
Svante Richter <svante.richter@gmail.com>
2024-12-16 15:09:12 +0100

    Update to latest from benchmarks

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const domCache = {}
-const cache = new WeakMap()
-export const e = new Proxy({}, { get: (_, t) => (...c) => (c[0] && typeof c[0] === 'object' && !c[0].t) ? { t, a: c[0], c: c.slice(1) } : { t, c } })
+export const e = new Proxy({}, { get: (_, t) => (a = null, ...c) => ({ t, c, a }) })
 export const r = (object, current = document.documentElement) => {
   if (object.a) {
     for (const key in object.a) {
-      if (key === 'key' || key === 'cache' || ('' + object.a[key]) === ('' + current.getAttribute(key))) continue
+      if (key === 'template' || key === 'opaque' || (key[0] === 'o' && key[1] === 'n' && current[key] === object.a[key]) || ('' + object.a[key]) === ('' + current.getAttribute(key))) continue
       if (key[0] === 'o' && key[1] === 'n') { // Event listener
         current[key] = object.a[key]
         continue
@@ -19,43 +17,51 @@ export const r = (object, current = document.documentElement) => {
       }
     }
   }
-  if (!object.c.length && current.childNodes.length) current.textContent = ''
-  if (!object.c.length) return
-  for (const i in object.c) {
+  if (current.hasChildNodes()) {
+    if (!object.c.length) {
+      current.textContent = ''
+      return
+    }
+  } else {
+    if (!object.c.length) return
+    const newChildren = []
+    for (let i = 0; i < object.c.length; i++) {
+      const c = object.c[i]
+      if (typeof c === 'string') {
+        newChildren.push(c)
+        continue
+      }
+      const _current = c.a && c.a.template ? c.a.template.cloneNode(true) : document.createElement(c.t)
+      newChildren.push(_current)
+      r(c, _current)
+    }
+    current.replaceChildren(...newChildren)
+    return
+  }
+  for (let i = 0; i < object.c.length; i++) {
     const c = object.c[i]
-    let p = current.childNodes[i] || null
-    if (typeof c === 'string' || typeof c === 'number') {
+    let p = current.childNodes[i]
+    let _current = p
+    if (typeof c === 'string') {
       if (!p || p.nodeName !== '#text') {
-        p ? current.replaceChild(document.createTextNode(c), p) : current.appendChild(document.createTextNode(c))
-      } else if (p.nodeValue !== (typeof c === 'string' ? c : '' + c)) {
+        _current = document.createTextNode(c)
+        p ? current.replaceChild(_current, p) : current.appendChild(_current)
+      } else if (p.nodeValue !== c) {
         p.nodeValue = c
       }
       continue
     }
-    if (c.a?.cached && cache.has(c)) {
-      if (cache.get(c) === p) continue
-      let _current = cache.get(c).cloneNode(true)
-      p ? current.replaceChild(_current, p) : current.appendChild(_current)
-      continue
-    }
-    if (c.a?.key && cache.has(c.a.key)) {
-      if (cache.get(c.a.key) === p) continue
-      let _current = cache.get(c.a.key)
-      p ? current.replaceChild(_current, p) : current.appendChild(_current)
-      p = _current
-      continue
-    }
+    if (c.a && c.a.opaque && p) continue
     if (!p || p.localName !== c.t) {
-      let _current = (domCache[c.t] || (domCache[c.t] = document.createElement(c.t))).cloneNode(false)
-      c.a?.cached && cache.set(c, _current)
-      c.a?.key && cache.set(c.a.key, _current) && cache.set(_current, c.a.key)
+      _current = c.a && c.a.template ? c.a.template.cloneNode(true) : document.createElement(c.t)
       p ? current.replaceChild(_current, p) : current.appendChild(_current)
-      r(c, _current)
-      continue
     }
-    r(c, p)
+    r(c, _current)
   }
-  while (current.childNodes.length > object.c.length) {
-    current.removeChild(current.childNodes[current.childNodes.length - 1])
+  if (current.childNodes.length > object.c.length) {
+    const rg = document.createRange()
+    rg.setStartBefore(current.children[object.c.length])
+    rg.setEndAfter(current.lastElementChild)
+    rg.deleteContents()
   }
 }
